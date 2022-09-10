a5d97e20bf3b271aefd18fc015936a333250fe8f
“David” <davidc2911@gmail.com>
2022-09-10 00:39:03 -0300

    Specify readable name for Tournament model's field

--- prode_qatar_2022/tournaments/models.py
+++ prode_qatar_2022/tournaments/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Tournament(models.Model):
 	name = models.CharField(max_length=32)
-	qty_teams = models.PositiveSmallIntegerField(default=20)
+	qty_teams = models.PositiveSmallIntegerField("Quantity Teams",default=20)
 
 	def __str__(self):
 		return f"{self.name}"
