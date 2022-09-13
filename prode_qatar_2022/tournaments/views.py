fa49ef48c9cb7234f77e9507318cf14aaf3b8967
“David” <davidc2911@gmail.com>
2022-09-12 21:21:44 -0300

    Fix errors for duplications in db when saving pronostics

--- prode_qatar_2022/tournaments/views.py
+++ prode_qatar_2022/tournaments/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from tournaments.models import Game, Pronostic
 from tournaments.forms import TeamForm, TournamentForm, GameForm, PronosticForm
+from commons.utils import querydict_to_dict
 
 
 def new_tournament(request):
@@ -49,22 +50,27 @@ def get_games_list(request):
 def do_pronostic(request):
 	if request.method == "POST":
 		num_games = Game.objects.all().count()
-		ddnew = {k: v[0] if len(v) == 1 else v for k, v in request.POST.lists()}
+		form_data = querydict_to_dict(request.POST)
 		for num in range(num_games):
-			pronostic = {"game": ddnew.get("pronostic_game")[num],"home_goals": ddnew.get("home_goals")[num], "away_goals": ddnew.get("away_goals")[num]}
-			form = PronosticForm(pronostic)
-			if form.is_valid():
-				form.save()
+			pronostic_data = {"game": int(form_data.get("pronostic_game")[num]),"home_goals": int(form_data.get("home_goals")[num]), "away_goals": int(form_data.get("away_goals")[num])}
+			pronostic = Pronostic.objects.filter(game_id=pronostic_data.get("game")).first()
+			if pronostic:
+				pronostic.home_goals = pronostic_data.get("home_goals")
+				pronostic.away_goals = pronostic_data.get("away_goals")
+				pronostic.save()
 			else:
-				print(form.errors.as_data())
+				form = PronosticForm(pronostic_data)
+				if form.is_valid():
+					form.save()
+				else:
+					print(form.errors.as_data())
 		return redirect("do_pronostic")
 	else:
 		games = Game.objects.all()
 		pronostics = []
 		for game in games:
-			try:
-				pronostic = Pronostic.objects.get(game_id=game.id)
-			except Pronostic.DoesNotExist:
+			pronostic = Pronostic.objects.filter(game_id=game.id).first()
+			if not pronostic:
 				pronostic = Pronostic(game=game)
 			pronostics.append(pronostic)
 		forms = [PronosticForm(instance=pronostic) for pronostic in pronostics]
