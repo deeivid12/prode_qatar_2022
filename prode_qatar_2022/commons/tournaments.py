edadd20f8b98fdf68f03aad6be0d342d484bd6be
“David” <davidc2911@gmail.com>
2022-10-05 19:45:03 -0300

    Fix code format following python conventions

--- prode_qatar_2022/commons/tournaments.py
+++ prode_qatar_2022/commons/tournaments.py
@@ -2,112 +2,133 @@ from tournaments.forms import PronosticForm
 from tournaments.models import Game, Pronostic
 from django.contrib.auth.models import User
 from django.db.models import Sum
-from commons.utils import querydict_to_dict, is_correct_same_result, is_correct_different_result, POINTS_CORRECT_SAME_RESULT, POINTS_CORRECT_DIFF_RESULT, POINTS_INCORRECT_RESULT
+from commons.utils import (
+    querydict_to_dict,
+    is_correct_same_result,
+    is_correct_different_result,
+    POINTS_CORRECT_SAME_RESULT,
+    POINTS_CORRECT_DIFF_RESULT,
+    POINTS_INCORRECT_RESULT,
+)
 
 
 def get_penalties_win(form_data):
-	all_values = [{k[-1]: v} for k,v in form_data.items() if k.startswith("ko_win_")]
-	penalties = []
-	for game_id in form_data.get("pronostic_game"):
-		for value in all_values:
-			if game_id in value:
-				penalties.append(value.get(game_id))
-				break
-			else:
-				penalties.append("0")
-	return penalties
+    all_values = [{k[-1]: v} for k, v in form_data.items() if k.startswith("ko_win_")]
+    penalties = []
+    for game_id in form_data.get("pronostic_game"):
+        for value in all_values:
+            if game_id in value:
+                penalties.append(value.get(game_id))
+                break
+            else:
+                penalties.append("0")
+    return penalties
 
 
 def get_do_pronostic_data(request_data):
-	"""It takes sent data and it processes it to return it clean and ready to work
+    """It takes sent data and it processes it to return it clean and ready to work
 
-	Args:
-		request_data (query dict dictionary): Request data from POST
+    Args:
+            request_data (query dict dictionary): Request data from POST
 
-	Returns:
-		dict: Clean and ready data to work
-	"""
-	data = querydict_to_dict(request_data)
-	penalties_data = get_penalties_win(data)
-	data["penalties_win"] = penalties_data
-	return data
+    Returns:
+            dict: Clean and ready data to work
+    """
+    data = querydict_to_dict(request_data)
+    penalties_data = get_penalties_win(data)
+    data["penalties_win"] = penalties_data
+    return data
 
 
 def get_all_pronostics_by_user_and_room(user, room):
-	"""Get all pronostics for an user, a room and a tournament_id given.
+    """Get all pronostics for an user, a room and a tournament_id given.
 
-	Args:
-		user (User model instance): User model instance with important information
-		room (Room model instance): Room model instance with important information
+    Args:
+            user (User model instance): User model instance with important information
+            room (Room model instance): Room model instance with important information
 
-	Returns:
-		list: List of pronostics
-	"""
-	games = Game.objects.filter(tournament_id=room.tournament_id)
-	pronostics = []
-	for game in games:
-		pronostic = Pronostic.objects.filter(game_id=game.id, user_id=user.id, room_id=room.id).first()
-		if not pronostic:
-			pronostic = Pronostic(game=game, user=user, room=room)
-		pronostics.append(pronostic)
-	return pronostics
+    Returns:
+            list: List of pronostics
+    """
+    games = Game.objects.filter(tournament_id=room.tournament_id)
+    pronostics = []
+    for game in games:
+        pronostic = Pronostic.objects.filter(
+            game_id=game.id, user_id=user.id, room_id=room.id
+        ).first()
+        if not pronostic:
+            pronostic = Pronostic(game=game, user=user, room=room)
+        pronostics.append(pronostic)
+    return pronostics
 
-				
+
 def update_pronostic(pronostic, pronostic_data):
-	"""Update pronostic instance (Pronostic model) with provided data
+    """Update pronostic instance (Pronostic model) with provided data
 
-	Args:
-		pronostic (Pronostic model instance): Pronostic model instance
-		pronostic_data (dict): Provided data like home_goals, away_goals, etc, to update the pronostic
-	"""
-	pronostic.home_goals = pronostic_data.get("home_goals")
-	pronostic.away_goals = pronostic_data.get("away_goals")
-	if pronostic.game.is_knockout:
-		pronostic.penalties_win = pronostic_data.get("penalties_win")
-	pronostic.save()
+    Args:
+            pronostic (Pronostic model instance): Pronostic model instance
+            pronostic_data (dict): Provided data like home_goals, away_goals, etc, to update the pronostic
+    """
+    pronostic.home_goals = pronostic_data.get("home_goals")
+    pronostic.away_goals = pronostic_data.get("away_goals")
+    if pronostic.game.is_knockout:
+        pronostic.penalties_win = pronostic_data.get("penalties_win")
+    pronostic.save()
+
 
 def new_pronostic_by_form(pronostic_data):
-	"""Creates a new pronostic instance by a form and provided data
+    """Creates a new pronostic instance by a form and provided data
 
-	Args:
-		pronostic_data (dict): Provided data like home_goals, away_goals, etc, to update the pronostic
-	"""
-	form = PronosticForm(pronostic_data)
-	if form.is_valid():
-		form.save()
-	else:
-		print(form.errors.as_data())
+    Args:
+            pronostic_data (dict): Provided data like home_goals, away_goals, etc, to update the pronostic
+    """
+    form = PronosticForm(pronostic_data)
+    if form.is_valid():
+        form.save()
+    else:
+        print(form.errors.as_data())
+
 
 def check_pronostics_results():
-	"""Check pronostics versus games results and it updates status and pronostics points
-	"""
-	pronostics = Pronostic.objects.filter(checked=False)
-	for pronostic in pronostics:
-		game = Game.objects.filter(id=pronostic.game.id).first()
-		if is_correct_same_result(pronostic, game):
-			points = POINTS_CORRECT_SAME_RESULT
-		elif is_correct_different_result(pronostic, game):
-			points = POINTS_CORRECT_DIFF_RESULT
-		else:
-			points = POINTS_INCORRECT_RESULT
-		pronostic.checked = True
-		pronostic.points = points
-		pronostic.save()
+    """Check pronostics versus games results and it updates status and pronostics points"""
+    pronostics = Pronostic.objects.filter(checked=False)
+    for pronostic in pronostics:
+        game = Game.objects.filter(id=pronostic.game.id).first()
+        if is_correct_same_result(pronostic, game):
+            points = POINTS_CORRECT_SAME_RESULT
+        elif is_correct_different_result(pronostic, game):
+            points = POINTS_CORRECT_DIFF_RESULT
+        else:
+            points = POINTS_INCORRECT_RESULT
+        pronostic.checked = True
+        pronostic.points = points
+        pronostic.save()
 
 
 def get_ranking_by_room(room_id):
-	"""Gets ranking of users, for a room id given, in ascending order
+    """Gets ranking of users, for a room id given, in ascending order
 
-	Args:
-		room_id (int): Room id to calculate the ranking
+    Args:
+            room_id (int): Room id to calculate the ranking
 
-	Returns:
-		list: List of dicts with the following information: position, username and total
-	"""
-	pronostics_ranking = Pronostic.objects.values('user_id').filter(checked=True, room_id=room_id).annotate(total=Sum('points')).order_by('-total')
-	ranking = []
-	for idx, pronostic in enumerate(pronostics_ranking):
-		username = User.objects.filter(id=pronostic.get('user_id')).first().username
-		position = idx+1
-		ranking.append({'position': position, 'username': username, 'total':pronostic.get('total')})
-	return ranking
+    Returns:
+            list: List of dicts with the following information: position, username and total
+    """
+    pronostics_ranking = (
+        Pronostic.objects.values("user_id")
+        .filter(checked=True, room_id=room_id)
+        .annotate(total=Sum("points"))
+        .order_by("-total")
+    )
+    ranking = []
+    for idx, pronostic in enumerate(pronostics_ranking):
+        username = User.objects.filter(id=pronostic.get("user_id")).first().username
+        position = idx + 1
+        ranking.append(
+            {
+                "position": position,
+                "username": username,
+                "total": pronostic.get("total"),
+            }
+        )
+    return ranking
--- prode_qatar_2022/tournaments/views.py
+++ prode_qatar_2022/tournaments/views.py
@@ -1,110 +1,135 @@
-from django.shortcuts import render, redirect
 from tournaments.models import Game, Pronostic
 from tournaments.forms import TeamForm, TournamentForm, GameForm, PronosticForm
-from commons.tournaments import get_all_pronostics_by_user_and_room, update_pronostic, new_pronostic_by_form, get_do_pronostic_data, check_pronostics_results, get_ranking_by_room
+from commons.tournaments import (
+    get_all_pronostics_by_user_and_room,
+    update_pronostic,
+    new_pronostic_by_form,
+    get_do_pronostic_data,
+    check_pronostics_results,
+    get_ranking_by_room,
+)
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 
+
 def new_tournament(request):
-	if request.method == "POST":
-		form = TournamentForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect("new_tournament")
-	else:
-		form = TournamentForm()
-		data = {"form": form, "title": "Tournament"}
-	return render(request, "tournaments/new.html", data)
+    if request.method == "POST":
+        form = TournamentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("new_tournament")
+    else:
+        form = TournamentForm()
+        data = {"form": form, "title": "Tournament"}
+    return render(request, "tournaments/new.html", data)
 
 
 def new_team(request):
-	if request.method == "POST":
-		form = TeamForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect("new_team")
-	else:
-		form = TeamForm()
-		data = {"form": form, "title": "Team"}
-	return render(request, "tournaments/new.html", data)
+    if request.method == "POST":
+        form = TeamForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("new_team")
+    else:
+        form = TeamForm()
+        data = {"form": form, "title": "Team"}
+    return render(request, "tournaments/new.html", data)
 
 
 def new_game(request):
-	if request.method == "POST":
-		form = GameForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect("new_game")
-	else:
-		form = GameForm()
-		data = {"form": form, "title": "Game"}
-	return render(request, "tournaments/new.html", data)
+    if request.method == "POST":
+        form = GameForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("new_game")
+    else:
+        form = GameForm()
+        data = {"form": form, "title": "Game"}
+    return render(request, "tournaments/new.html", data)
 
 
 def get_games_list(request):
-	games = Game.objects.all()
-	data = {"games": games, "title": "Todos los partidos"}
-	return render(request, "tournaments/games_list.html",
-            data)
+    games = Game.objects.all()
+    data = {"games": games, "title": "Todos los partidos"}
+    return render(request, "tournaments/games_list.html", data)
 
 
 def do_pronostic(request, room_id):
-	# need to be authenticated, otherwise it won't work
-	current_user = request.user
-	room = current_user.tournaments_rooms.filter(id=room_id).first()
-	if request.method == "POST":
-		num_games = Game.objects.filter(tournament_id=room.tournament_id).count()
-		form_data = get_do_pronostic_data(request.POST)
-		for num in range(num_games):
-			pronostic_data = {"game": int(form_data.get("pronostic_game")[num]),"home_goals": int(form_data.get("home_goals")[num]), "away_goals": int(form_data.get("away_goals")[num]), "penalties_win": int(form_data.get("penalties_win")[num]), "user":current_user, "room":room}
-			pronostic = Pronostic.objects.filter(game_id=pronostic_data.get("game"), user_id=current_user.id, room_id=room_id).first()
-			if pronostic and pronostic.checked:
-				break
-			if pronostic:
-				update_pronostic(pronostic, pronostic_data)
-			else:
-				new_pronostic_by_form(pronostic_data)
-		return redirect("do_pronostic", room_id=room_id)
-	else:
-		pronostics = get_all_pronostics_by_user_and_room(current_user, room)
-		forms = [PronosticForm(instance=pronostic) for pronostic in pronostics]
-		# forms and games have the same size
-		data = {"forms_pronostics": zip(forms, pronostics), "title": "Realizar Pronosticos"}
-		return render(request, "tournaments/do_pronostic.html",
-				data)
+    # need to be authenticated, otherwise it won't work
+    current_user = request.user
+    room = current_user.tournaments_rooms.filter(id=room_id).first()
+    if request.method == "POST":
+        num_games = Game.objects.filter(tournament_id=room.tournament_id).count()
+        form_data = get_do_pronostic_data(request.POST)
+        for num in range(num_games):
+            pronostic_data = {
+                "game": int(form_data.get("pronostic_game")[num]),
+                "home_goals": int(form_data.get("home_goals")[num]),
+                "away_goals": int(form_data.get("away_goals")[num]),
+                "penalties_win": int(form_data.get("penalties_win")[num]),
+                "user": current_user,
+                "room": room,
+            }
+            pronostic = Pronostic.objects.filter(
+                game_id=pronostic_data.get("game"),
+                user_id=current_user.id,
+                room_id=room_id,
+            ).first()
+            if pronostic and pronostic.checked:
+                break
+            if pronostic:
+                update_pronostic(pronostic, pronostic_data)
+            else:
+                new_pronostic_by_form(pronostic_data)
+        return redirect("do_pronostic", room_id=room_id)
+    else:
+        pronostics = get_all_pronostics_by_user_and_room(current_user, room)
+        forms = [PronosticForm(instance=pronostic) for pronostic in pronostics]
+        # forms and games have the same size
+        data = {
+            "forms_pronostics": zip(forms, pronostics),
+            "title": "Realizar Pronosticos",
+        }
+        return render(request, "tournaments/do_pronostic.html", data)
+
 
 def check_pronostics(request):
-	check_pronostics_results()
-	return redirect('all_games')
+    check_pronostics_results()
+    return redirect("all_games")
 
 
 def get_points(request):
-	# it'll work just for now, to test everything is good
-	pronostics = Pronostic.objects.filter(checked=True)
-	points = [pronostic.points for pronostic in pronostics]
-	print(sum(points))
-	return redirect('all_games')
+    # it'll work just for now, to test everything is good
+    pronostics = Pronostic.objects.filter(checked=True)
+    points = [pronostic.points for pronostic in pronostics]
+    print(sum(points))
+    return redirect("all_games")
 
 
 def get_ranking(request, room_id):
-	current_user = request.user
-	user_rooms_ids = User.objects.filter(id=current_user.id).first().tournaments_rooms.values_list('id', flat=True)
-	if room_id not in user_rooms_ids:
-		return JsonResponse({"error_404": "No corresponde el room con el usuario"})
-	ranking = get_ranking_by_room(room_id)
-	data = {"pronostics_ranking": ranking}
-	return render(request, "tournaments/pronostics_ranking.html", data)
+    current_user = request.user
+    user_rooms_ids = (
+        User.objects.filter(id=current_user.id)
+        .first()
+        .tournaments_rooms.values_list("id", flat=True)
+    )
+    if room_id not in user_rooms_ids:
+        return JsonResponse({"error_404": "No corresponde el room con el usuario"})
+    ranking = get_ranking_by_room(room_id)
+    data = {"pronostics_ranking": ranking}
+    return render(request, "tournaments/pronostics_ranking.html", data)
 
 
 def get_rooms_list_by_user(request):
-	current_user = request.user
-	rooms = current_user.tournaments_rooms.all()
-	data = {"rooms": rooms}
-	return render(request, "tournaments/rooms_list.html", data)
+    current_user = request.user
+    rooms = current_user.tournaments_rooms.all()
+    data = {"rooms": rooms}
+    return render(request, "tournaments/rooms_list.html", data)
 
 
 def get_room(request, id):
-	current_user = request.user
-	room = current_user.tournaments_rooms.filter(id=id).first()
-	data = {"room": room}
-	return render(request, "tournaments/room_detail.html", data)
+    current_user = request.user
+    room = current_user.tournaments_rooms.filter(id=id).first()
+    data = {"room": room}
+    return render(request, "tournaments/room_detail.html", data)
