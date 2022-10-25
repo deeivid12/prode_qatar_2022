236f43e62d28a98daf53c98c5298b2c68e604ef9
“David” <davidc2911@gmail.com>
2022-10-25 01:45:11 -0300

    Enabled/disabled penalties radio button with js

--- prode_qatar_2022/tournaments/forms.py
+++ prode_qatar_2022/tournaments/forms.py
@@ -1,41 +1,45 @@
 from django import forms
 from tournaments.models import Team, Tournament, Game, Pronostic
 
+
 class TeamForm(forms.ModelForm):
     class Meta:
         model = Team
-        fields = '__all__'
-        widgets = {
-			'name': forms.TextInput(attrs={'class': 'form-control'})
-		}
+        fields = "__all__"
+        widgets = {"name": forms.TextInput(attrs={"class": "form-control"})}
 
 
 class TournamentForm(forms.ModelForm):
     class Meta:
         model = Tournament
-        fields = '__all__'
+        fields = "__all__"
         widgets = {
-			'name': forms.TextInput(attrs={'class': 'form-control'}),
-			'qty_teams': forms.NumberInput(attrs={'class': 'form-control'})
-		}
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "qty_teams": forms.NumberInput(attrs={"class": "form-control"}),
+        }
 
 
 class GameForm(forms.ModelForm):
-	class Meta:
-		model = Game
-		exclude = ['home_goals', 'away_goals']
-		widgets = {
-			'home_team': forms.TextInput(attrs={'class': 'form-control'}),
-			'away_team': forms.TextInput(attrs={'class': 'form-control'}),
-			'tournament': forms.Select(attrs={'class': 'form-control'}),
-		}
+    class Meta:
+        model = Game
+        exclude = ["home_goals", "away_goals"]
+        widgets = {
+            "home_team": forms.TextInput(attrs={"class": "form-control"}),
+            "away_team": forms.TextInput(attrs={"class": "form-control"}),
+            "tournament": forms.Select(attrs={"class": "form-control"}),
+        }
 
 
 class PronosticForm(forms.ModelForm):
-	class Meta:
-		model = Pronostic
-		fields = '__all__'
-		widgets = {
-			'home_goals': forms.NumberInput(attrs={'class': 'form-control'}),
-			'away_goals': forms.NumberInput(attrs={'class': 'form-control'}),
-		}
+    class Meta:
+        model = Pronostic
+        fields = "__all__"
+        widgets = {
+            "home_goals": forms.NumberInput(
+                attrs={
+                    "class": "form-control",
+                    "oninput": "check_tie(this)",
+                }
+            ),
+            "away_goals": forms.NumberInput(attrs={"class": "form-control", "oninput": "check_tie(this)",}),
+        }
