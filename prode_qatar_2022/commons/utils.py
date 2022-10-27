bd1acd54200c520699b1f4098d967c1bb6f57f12
“David” <davidc2911@gmail.com>
2022-10-27 18:14:05 -0300

    Refactor check_pronostics logic and add the cases for knockout games

--- prode_qatar_2022/commons/utils.py
+++ prode_qatar_2022/commons/utils.py
@@ -4,20 +4,32 @@ POINTS_CORRECT_SAME_RESULT = 5
 POINTS_CORRECT_DIFF_RESULT = 3
 POINTS_INCORRECT_RESULT = 0
 
-def querydict_to_dict(query_dict):
-	return {k: v[0] if len(v) == 1 else v for k, v in query_dict.lists()}
 
+def querydict_to_dict(query_dict):
+    return {k: v[0] if len(v) == 1 else v for k, v in query_dict.lists()}
 
 
 def is_correct_same_result(pronostic, game):
-	if (pronostic.home_goals, pronostic.away_goals) == (game.home_goals, game.away_goals):
-		return True
+    result = (pronostic.home_goals, pronostic.away_goals) == (
+        game.home_goals,
+        game.away_goals,
+    )
+    if game.is_knockout:
+        penalties = game.penalties_win == pronostic.penalties_win
+        return result and penalties
+    return result
 
 
 def is_correct_different_result(pronostic, game):
-	if (pronostic.home_goals > pronostic.away_goals) and (game.home_goals > game.away_goals):
-		return True
-	elif (pronostic.home_goals < pronostic.away_goals) and (game.home_goals < game.away_goals):
-		return True
-	elif (pronostic.home_goals == pronostic.away_goals) and (game.home_goals == game.away_goals):
-		return True
+    result = (
+        (pronostic.home_goals > pronostic.away_goals)
+        and (game.home_goals > game.away_goals)
+        or (pronostic.home_goals < pronostic.away_goals)
+        and (game.home_goals < game.away_goals)
+        or (pronostic.home_goals == pronostic.away_goals)
+        and (game.home_goals == game.away_goals)
+    )
+    if game.is_knockout:
+        penalties = game.penalties_win == pronostic.penalties_win
+        return result and penalties
+    return result
