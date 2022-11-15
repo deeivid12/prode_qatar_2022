8466b10834239df9d2d9d645223a53349e82f165
“David” <davidc2911@gmail.com>
2022-11-14 22:20:11 -0300

    Add CSRF trusted origins to settings

--- prode_qatar_2022/prode_qatar_2022/settings.py
+++ prode_qatar_2022/prode_qatar_2022/settings.py
@@ -77,11 +77,21 @@ WSGI_APPLICATION = "prode_qatar_2022.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # },
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "railway",
+        "USER": "root",
+        "PASSWORD": "FBSak8A8wNHHY8mkXgyY",
+        "HOST": "containers-us-west-121.railway.app",
+        "PORT": "5765",
     }
 }
+
+CSRF_TRUSTED_ORIGINS = ["https://prodeqatar2022-production.up.railway.app/"]
 
 
 # Password validation
