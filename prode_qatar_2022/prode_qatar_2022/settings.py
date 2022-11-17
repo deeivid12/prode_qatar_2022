0cbe3c980ce2b57aa5e9fc5035fcff4c08ef388d
“David” <davidc2911@gmail.com>
2022-11-17 12:18:02 -0300

    Set debug=False as default in settings.py

--- prode_qatar_2022/prode_qatar_2022/settings.py
+++ prode_qatar_2022/prode_qatar_2022/settings.py
@@ -25,6 +25,11 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
