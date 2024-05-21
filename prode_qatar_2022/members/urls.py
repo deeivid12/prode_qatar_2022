77a4c1ae1c00d69cdc61d0071cdce1a26298fb28
deeivid12 <deeivid12@gmail.com>
2024-05-21 16:01:39 -0300

    Add view and template to change the password within the app (#5)

--- prode_qatar_2022/members/urls.py
+++ prode_qatar_2022/members/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from members import views
 
@@ -5,4 +6,5 @@ urlpatterns = [
     path("login", views.login_user, name="login"),
     path("logout_user", views.logout_user, name="logout"),
     path("register", views.register_user, name="register"),
+    path("password", views.PasswordChangeView.as_view(template_name='auth/change-password.html'), name="change_password"),
 ]
