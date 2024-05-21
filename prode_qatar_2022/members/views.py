4cd4640b8a47c5c262a58ea90eadb12be672aebf
David <deeivid12@gmail.com>
2024-05-21 14:30:16 -0300

    Add view and template to change the password within the app

--- prode_qatar_2022/members/views.py
+++ prode_qatar_2022/members/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
+from django.urls import reverse_lazy
 
 
 def login_user(request):
@@ -48,3 +50,15 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("Sesión cerrada correctamente"))
     return redirect("login")
+
+class PasswordChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('welcome')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Ha cambiado su password correctamente')
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, 'Error al cambiar password')
+        return super().form_invalid(form)
