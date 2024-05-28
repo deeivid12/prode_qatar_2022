54d64dd22bdc466945217a633a9b87b5e8039c62
deeivid12 <deeivid12@gmail.com>
2024-05-28 12:27:43 -0300

    Add changes to Games admin panel (#7)

--- prode_qatar_2022/tournaments/admin.py
+++ prode_qatar_2022/tournaments/admin.py
@@ -1,8 +1,40 @@
 from django.contrib import admin
-from tournaments import models
+from tournaments import forms, models
 
-admin.site.register(models.Tournament)
+
+class GameAdmin(admin.ModelAdmin):
+    form = forms.GameAdminForm
+
+    fieldsets = (
+        (None, {
+            'fields': ('tournament', 'game_instance', 'date_time')
+        }),
+        ('Teams', {
+            'fields': ('home_team', 'away_team')
+        }),
+        ('Match Details', {
+            'fields': ('is_knockout', 'penalties_win', 'home_goals', 'away_goals', 'played')
+        }),
+    )
+
+    list_display = (
+        'get_game_display',
+        'tournament',
+        'game_instance',
+        'home_team',
+        'away_team',
+        'is_knockout',
+        'penalties_win',
+        'date_time',
+        'played'
+    )
+
+    @admin.display(description='Match')
+    def get_game_display(self, obj):
+        return str(obj)
+
 admin.site.register(models.Team)
-admin.site.register(models.Game)
+admin.site.register(models.Game, GameAdmin)
+admin.site.register(models.Tournament)
 admin.site.register(models.Pronostic)
 admin.site.register(models.Room)
