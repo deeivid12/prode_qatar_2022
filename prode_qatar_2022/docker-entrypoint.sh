d9c170a7f8a8edabaa26c4a0f27f1391f7156fd0
deeivid12 <deeivid12@gmail.com>
2024-06-06 12:24:17 -0300

    Configure envvars and static files (#12)
    * Set envvars and create a sample file as example. Configure whitenoise for static files
    
    * Add production mode for docker
    
    * Remove command in entrypoint
    
    * Deactivate docker files to dockerize in railway. Also add python version to run in runtime.txt

---
+++ prode_qatar_2022/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+if [ "$ENVIRONMENT" = "DEV" ]; then
+  echo 'Running in development mode...'
+  exec python manage.py runserver 0.0.0.0:8000
+else
+  echo 'Running collectstatic...'
+  python manage.py collectstatic --no-input
+
+  echo 'Applying migrations...'
+  python manage.py migrate
+
+  echo 'Running server...'
+  exec gunicorn prode_qatar_2022.wsgi:application --bind 0.0.0.0:$PORT
+fi
